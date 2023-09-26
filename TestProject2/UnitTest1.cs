92d08d5567633979446e67800495fe6ce7b70b2c
Christoffer Ottestig <98085034+9Carat@users.noreply.github.com>
2023-09-26 09:37:40 +0200

    Update #2

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using NET22_WebApp;
 
 namespace TestProject2
@@ -17,15 +18,25 @@ namespace TestProject2
             // Assert
         }
         [TestMethod]
-        public void VariablesTest_Returns_Error()
+        public void VariablesTest_Returns_Equal()
         {
             // Arrange
             Numbers num = new Numbers();
-            var actual = num.Add(10, 11);
+            var actual = num.Add(10, 12);
             var expected = 22;
             // Act
             Assert.AreEqual(expected, actual);
             // Assert
+        }
+        [TestMethod]
+        public void FluentAssertion_Test1()
+        {
+            string name = "Tobias";
+            //name.Should().StartWith("T").And.EndWith("s");
+            int number = 22;
+            //number.Should().BeLessThan(50);
+            List<string> list = new List<string>() { "Ante", "Reidar", "Harry"};
+            list.Should().HaveCount(3);
         }
     }
 }
